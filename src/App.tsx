49851f3ca64a4f6f6d2efa705bf1da74ceb40601
Ross Gardiner <github@public.rossng.eu>
2021-10-26 18:07:52 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Repro: graphql-code-generator + urql</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
